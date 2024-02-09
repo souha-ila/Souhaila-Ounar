77076c32317ab94ce5b79337143926252c734497
souha-ila <ounarsouhaila23@gmail.com>
2024-02-09 23:25:34 +0100

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,52 @@
+// blog data
+
+export const Blogs = [
+  {
+    id: 1,
+    name: "CatZoom is a user-friendly web platform facilitating cat adoption through detailed cat profiles and an administrative interface.",
+    tags: ["Bootstrap", "spring boot", "JPA", "JSP", "mySql"],
+
+    imgSrc: "https://i.ibb.co/8DS7KcG/1.png",
+    link: "https://github.com/souha-ila/CatZoom-main",
+  },
+  {
+    id: 2,
+    name: "LirePLUS is a virtual literary haven offering an immersive reading experience with meticulously curated digital books.",
+    tags: ["Bootstrap", ".NET ", "Entity Framework", "SQL Server"],
+
+    imgSrc: "https://i.ibb.co/T245h6d/2.png",
+    link: "https://github.com/souha-ila/LibraryFinal-main",
+  },
+  {
+    id: 3,
+    name: "Yarn Vibes is an eco-responsible online sales application specializing in crochet products",
+    tags: ["Symfony", "Twig", "MySql"],
+
+    imgSrc: "https://i.ibb.co/mHZtfFd/11.png",
+    link: "https://github.com/souha-ila/Symfony_Store-main.git",
+  },
+  {
+    id: 4,
+    name: "UEMF News is an application developed with Node.js and MongoDB, allowing you to follow university news.",
+    tags: ["React", "Node.js", "Express", "MongoDB"],
+
+    imgSrc: "https://i.ibb.co/RY4g2Jw/3.png",
+    link: "https://github.com/souha-ila/MERN_App",
+  },
+  {
+    id: 5,
+    name: "Vroom is a comprehensive website, offering a convenient car rental experience for customers.",
+    tags: ["Laravel", "mysql", "bootstrap"],
+
+    imgSrc: "https://i.ibb.co/8zTvPyx/4.png",
+    link: "https://github.com/souha-ila/depoVroo--main/tree/main",
+  },
+  {
+    id: 6,
+    name: "MOTSUC offers a unique blend of hand-painted treasures,providing an elevated experience in artistic expression.",
+    tags: ["html", "css", "JavaScript"],
+
+    imgSrc: "https://i.ibb.co/Wnnq65P/5.png",
+    link: "https://souha-ila.github.io/MOTSUC/",
+  },
+];
